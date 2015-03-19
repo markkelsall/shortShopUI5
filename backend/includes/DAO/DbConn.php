a249abe00546f65e48f461c21b14164a15e43730
Mark Kelsall <mark.kelsall@gmail.com>
2015-03-19 23:15:03 +0000

    MK 19/03/2015 add check for server

--- backend/includes/DAO/DbConn.php
+++ backend/includes/DAO/DbConn.php
@@ -2,11 +2,15 @@
 class DbConn {
 	private $con = null;
 	
-	private $host = "mysql09.iomart.com"; //local: localhost live: mysql09.iomart.com
-	private $user = "kelshh177"; // local: root live: kelshh177
-	private $pass = "markr879"; //local: "" admin live: markr879
-	private $db = "kelshh177"; //local: shortShop live: kelshh177
+	private $host = "";
+	private $user = "";
+	private $pass = "";
+	private $db = "";
 	
+	public function __construct() {
+		$this->determineServer();
+	}
+
 	public function dbConnect () {
 		$con = new mysqli($this->host, $this->user, $this->pass, $this->db);
 		
@@ -22,6 +26,20 @@ class DbConn {
 	public function dbClose () {
 		//close database connection
 		$this->con->close();
+	}
+
+	private function determineServer () {
+		if($_SERVER['SERVER_NAME'] == 'localhost') {
+			$this->host = "localhost";
+			$this->user = "root";
+			$this->pass = "";
+			$this->db = "shortShop";
+		} else {
+			$this->host = "mysql09.iomart.com";
+			$this->user = "kelshh177";
+			$this->pass = "markr879";
+			$this->db = "kelshh177";
+		}
 	}
 }
 ?>
