582903c265bd91235d5189b65c7b1b70db176611
Mark Kelsall <mark.kelsall@gmail.com>
2015-07-23 15:51:44 +0100

    Remove unused utility files

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,22 +1,20 @@
 var bootstrap = {
-		
+
 		namespaces : [],
-		
+
 		jsFiles : [
 			 	"js.Formatter",
 			 	"js.utils.AjaxModel",
-			 	"js.utils.DateFormatter",
 			 	"js.utils.Logger",
 			 	"js.utils.Util",
-			 	"js.utils.EAO",
 			 	"js.utils.Navigation"
 	        ],
-	        
+
 	    cssFiles : [
 	    	"css/style.css"
 	    ],
-	    
+
 	    debugMode : "forcedTrue", //development = "forcedTrue", uat = "true", production = "false"
-	    
+
 	    logLevel : "error" //info, warn, debug, error
 };
